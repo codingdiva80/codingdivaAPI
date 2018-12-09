6f9b99dcd36d1ebf17d5068721190fe82a25386f
Joe Linstrum <jlinstrum@Joes-MacBook-Pro.local>
2018-12-08 21:14:12 -0800

    API updates

--- server.js
+++ server.js
@@ -3,30 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-// var MongoClient = require('mongodb').MongoClient
-//   , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost/codingdivadb';
-// Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server");
-
-//   db.close();
-// });
-try{
-  mongoose.connect(url);
-  console.log("Connected to mongodb");
-}
-catch(e){
-  console.log("Could not connect to db "+e);
-  process.exit(1);
-}
-
 
 // Setting up an Express app
 const app = express();
@@ -49,8 +25,10 @@ app.get('/', (req, res) => res.send('Hello!'));
 
 /* get routes */
 let userRoutes = require("./routes/user");
+let jobRoutes = require('./routes/jobs');
 app.use(userRoutes);
+app.use(jobRoutes);
 
-  app.listen(port, function() {
-    console.log("App listening on PORT " + port);
-  });
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
+});
